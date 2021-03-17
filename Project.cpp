f5c6e174ee4570175f02a7616c217aaf6dca1b2a
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:39:35 -0500

    Create Project.cpp

---
+++ Project.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+    
+    int one;
+    int two;
+    int three;
+    
+    char a;
+    char b;
+    char c;
+    
+    double dos;
+    
+    
+    std::cout << "Hello User" << std::endl;
+    
+    std::cout << "Enter: 1 2 3" << std::endl;
+    std::cin >> one >> two >> three;
+    
+    std::cout << "Enter: a b c" << std::endl;
+    std::cin >> a >> b >> c;
+    
+    std::cout << "Enter: 2.012" << std::endl;
+    std::cin >> dos;
+    
+    
+    std::cout << one << " " << two << " " << three << std::endl;
+    std::cout << a << b << c << std::endl;
+    std::cout << dos << std::endl;
+    
+    
+    
+    
+    return 0;
+    
+    
+    
+}
