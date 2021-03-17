a5b57bbbe50b3b94fb1499b53f229e47ce724dbd
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:35:07 -0500

    Create Basic_IO.cpp

---
+++ Basic_IO.cpp
@@ -0,0 +1,21 @@
+// Basic input and outputting, 6/20/19
+
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	 int num1;
+     int num2;
+     double num3;
+     
+     cout << "Please enter an integer here: " << endl;
+     cin >> num1;
+     cout << "You entered " << num1 << endl;
+    
+    return 0;
+}
